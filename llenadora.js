ab62ebcc05a71499018cc9f5e642e8db7805a4c3
Albert Mulà <skal88@me.com>
2015-09-10 15:46:10 +0200

    first commit

---
+++ llenadora.js
@@ -0,0 +1,127 @@
+var Llenadora = function(){
+	this.offsetIniXp = 0; // Distancia inicial hasta la primera botella fila par (eje X)
+	this.offsetIniXi = 0; // Distancia inicial hasta la primera botella fila inpar (eje X)
+	this.offsetIniY  = 0; // Distancia inicial hasta la primera botella (eje Y)
+
+	this.offsetXp = 0; // Distancia entre botellas fila par (eje X)
+	this.offsetXi = 0; // Distancia entre botellas fila inp (eje X)
+	this.offsetY  = 0; // Distancia entre botellas (eje Y)
+
+	this.lastXp = 0; // Distancia de la ultima botella fila par (eje X)
+	this.lastXi = 0; // Distancia de la ultima botella fila inpar (eje X)
+	this.lastY = 0; // Distancia de la ultima botella (eje Y);
+
+	this.cantBotellasP = 1; // Cantidad de botellas en las filas pares
+	this.cantBotellasI = 1; // Cantidad de botellas en las filas inpares
+	this.cantBotellasF = 1; // Cantidad de filas de botellas
+
+	this.configStatus = 0; // Estado de la configuracion. 0 = "Sin configurar"; 1 = "Configurando"; 2 = "Configurada";
+	this.configStatusPoint = 0; // Punto en el que se encuentra de la configuración
+
+	this.stepsLog = [];
+	this.actualStep = 0;
+
+	this.motorX = new Motor();
+	this.motorY = new Motor();
+}
+
+Llenadora.prototype.init = function(){
+	if(this.configStatus == 2){ // Si la maquina está configurada
+		this.offsetXp = (this.lastXp - this.offsetIniXp) / this.cantBotellasP - 1;
+		this.offsetXi = (this.lastXi - this.offsetIniXi) / this.cantBotellasI - 1;
+		this.offsetY  = (this.lastY  - this.offsetIniY ) / this.cantBotellasF - 1;
+
+
+		// Encendemos los motores 
+		this.motorX.enable = 1;
+		this.motorY.enable = 1;
+
+		// Generamos todas las posiciones
+		this.createPositionsLog();
+
+		return { message: "Maquina inicializada correctamente", code: 200 };
+	} else{
+		return { message: "Debes configurar la maquina antes", code: 300 };
+	}
+}
+
+Llenadora.prototype.createPositionsLog = function(){
+	var _X = this.offsetIniXp;
+	var _Y  = this.offsetIniY;
+	var filaPI = 0; // 0 = par; 1 = inpar;
+
+	for( var y=0; y<this.cantBotellasF; y++){ // Recorremos las filas
+
+		var cantPorFila = y%2 == 0 ? this.cantBotellasP : this.cantBotellasI;
+		for( var x=0; x<cantPorFila; x++ ){
+			var posBotella = {x : _X, y: _Y };
+			this.stepsLog.push(posBotella);
+			_X += y%2 == 0 ? this.offsetXp : this.offsetXi;
+		}
+		_Y += this.offsetY;
+		_X = y+1 % 2 == 0 ? this.offsetIniXp : this.offsetIniXi;
+		
+	}
+}
+
+Llenadora.prototype.move = function(){
+	this.motorX.move();
+	this.motorY.move();
+	if(this.stepsLog.length > 0 && this.configStatus == 2){
+		var newPos = this.stepsLog[this.actualStep];
+		this.moveTo( newPos.x, newPos.y ); // Vamos a la posicion de inicio
+	}
+}
+
+Llenadora.prototype.moveTo = function( x, y){
+	this.motorX.posDes = x;
+	this.motorY.posDes = y;
+}
+
+Llenadora.prototype.nextStep = function(){
+	if( this.actualStep >= 0 && this.actualStep < this.stepsLog.length -1 ){
+		this.actualStep++;
+		console.log(this.actualStep);
+	} else{
+		this.actualStep = 0;
+	}
+}
+
+Llenadora.prototype.resetMotors = function(){
+	this.motorX.reset();
+	this.motorY.reset();
+}
+
+
+
+
+
+
+
+
+
+var Motor = function(){
+	this.enable = 0; // 0 = Desactivado; 1 = Activado;
+	this.posAct = 0;
+	this.posDes = 0;
+	this.dir = 1; // 1 = Adelante; -1 = Atras;
+}
+
+Motor.prototype.move = function(){
+	if( this.posAct != this.posDes && this.enable == 1 ){
+		if( this.posDes > this.posAct ){ this.setDirection("FORWARD"); }
+		else if( this.posDes < this.posAct ){ this.setDirection("BACKWARD"); }
+
+		this.posAct += this.dir;
+	}
+}
+
+Motor.prototype.setDirection = function(dir){
+	if(dir == 1 || dir == "FORWARD"){ this.dir = 1; }
+	else if(dir == -1 || dir == "BACKWARD"){ this.dir = -1; }
+	else if(dir == 0  || dir == "STOP"){ this.dir = 0; }
+}
+
+Motor.prototype.reset = function(){
+	this.posDes = 0;
+}
